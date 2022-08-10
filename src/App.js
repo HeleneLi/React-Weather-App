bc721e0bb8bcd5f69457664c51ed356a4a1f1d6f
HeleneLi <89549373+HeleneLi@users.noreply.github.com>
2022-08-10 20:51:25 +0100

    upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello World!
+          <Weather/>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <h2>Search the city you want!</h2>
+  )
+}
