e1cfb4d9e733386c2ceeafbeba3461dfe57cc003
HeleneLi <89549373+HeleneLi@users.noreply.github.com>
2022-08-11 10:41:19 +0100

    github link added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <br></br>
+      <footer>
+        This project is created by Helene Lipp and is {" "}
+        <a href="https://github.com/HeleneLi/React-Weather-App" target="_blank">
+        open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
